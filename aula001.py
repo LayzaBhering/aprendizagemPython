ff0ed8de5c65a074f6442572fe5018fe6dd8a20d
LayzaBhering <layzabheringdeabreu@gmail.com>
2024-10-29 23:29:06 -0300

    Enviando o arquivo para a plataforma do GitHub

---
+++ aula001.py
@@ -0,0 +1,18 @@
+#Váriaveis + fstring
+
+#A váriavel é uma caixa que guarda um valor.
+#Esse valor pode ser resgatado e manipulado.
+
+nome = 'Python'
+
+fraseNome = f'Seu nome é... {nome}'
+
+#A váriavel nome recebe o valor dentro das aspas.
+#Ponto importante, lê-se "=" como recebe...
+#fraseNome (váriavel) recebe uma frase + a váriavel nome dentro de aspas. Observe que há um f antes da abertura da aspa.
+#Essa sintaxe possibilita que o programador manipule váriaveis dentro de strings.
+#Caso não tivesse essa fstring + { }, o nome seria apenas um texto dentro de fraseNome. Testa aí!
+
+print(fraseNome)
+
+#print é uma função de impressão. Pode ser útil para entender problemas de desenvolvimento e até a interagir com clientes.
